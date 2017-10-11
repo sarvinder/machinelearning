15956c1aea85e9ea3866e6ba3abc58a72f4dcf31
savio singh <savi895@gmail.com>
2017-10-11 15:04:44 +0000

    another commit

--- tensor.py
+++ tensor.py
@@ -1,10 +1,23 @@
 import tensorflow as tf
 
 
-
-a= tf.add(3,5)
+#a=tf.add(3,5)
 ##to get the value of a we need to run the session
 
-sess= tf.Session()
-print(sess.run(a))
-sess.close()
+#sess= tf.Session()
+#print(sess.run(a))
+#sess.close()
+#to get the random number matrix or vector
+#tf.truncated_normal()
+
+#This is used to visualize the data example
+def visualize():
+    a=tf.constant(2)
+    b=tf.constant(3)
+    x=tf.add(a,b)
+
+    with tf.Session() as sess:
+        writer=tf.summary.FileWriter('./graphs',sess.graph)
+        print(sess.run(x))
+    writer.close()
+    sess.close()
